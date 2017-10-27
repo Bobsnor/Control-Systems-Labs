08143ec5db83ce5e58c9edf72199219dbf4f0c82
Bobsnor <michielarts78@gmail.com>
2017-10-27 12:27:31 +0200

    sinus

--- Assignment II 5/assignment4.m
+++ Assignment II 5/assignment4.m
@@ -16,16 +16,29 @@ kp = 8e8;
 ki = 5e7;
 kd = 15e5;
 % C = 1.0618e9*(s+589.5)*(s+1.748)/s;
-C = kp + ki/s + kd*s;
+% C = kp + ki/s + kd*s;
 
-% C = C/((s+1e20)*(s+2e20)); % C + poles
+% C = C/((s+5e3)*(s+6e3)); % C + poles
+
+C = (8.4875e10*(s+120)*(s+0.1));
+C = C/(s*(s+1e04)*(s^2 + 1600*s + 6.724e05));       % sinus werkt goed 
+
+C = (1.681e11 *(s+120)*(s+0.05));
+C = C/(s*(s+1e04)*(s^2 + 1600*s + 6.724e05));       % sinus werkt ook goed 
+
+C = (1.9396e11*(s+130)*(s+0.04));
+C = C/(s*(s+1e04)*(s^2 + 1600*s + 6.724e05));       % sinus werkt ook goed 
+
+C = (2.72e11*(s+250)*(s+0.2));
+C = C/(s*(s+1e04)*(s^2 + 2000*s + 1.36e06));
+
 % pidTuner(LinModel);
 
 % load('Assignment4.mat');
 
 % Use rltool to design a Controller C
 sisotool(LinModel, C);
-% input('Press enter to continue after exporting the controller to workspace.');
+input('Press enter to continue after exporting the controller to workspace.');
 % After exporting a Controller C to workspace execute this part of the
 % script
 
